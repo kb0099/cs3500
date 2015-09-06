a4edbbe54bf0080218e1bfa80eafeeeae3e96486
Kedar B <pra_ked@live.com>
2015-09-06 12:11:49 -0600

    Add empty XML comments
    Adds empty XML documentation comments to class Evaluator, delegate Lookup, and method Evaluate.

--- PS1/FormulaEvaluator/Evaluator.cs
+++ PS1/FormulaEvaluator/Evaluator.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace FormulaEvaluator
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public static class Evaluator
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
         public delegate int Lookup(String v);
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="exp"></param>
+        /// <param name="variableEvaluator"></param>
+        /// <returns></returns>
         public static int Evaluate(String exp, Lookup variableEvaluator)
         {
             return 0;
