23cc4bcaf139d6757c9c30248b81ca4b2302f4eb
Kedar B <pra_ked@live.com>
2015-09-07 17:10:35 -0600

    Add IsValidVar method

--- PS1/FormulaEvaluator/Evaluator.cs
+++ PS1/FormulaEvaluator/Evaluator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace FormulaEvaluator
@@ -12,11 +13,17 @@ namespace FormulaEvaluator
     public static class Evaluator
     {
         /// <summary>
+        /// Represents a valid pattern for variable name or identifier.
+        /// </summary>
+        const String VALID_VAR_PATTERN = @"^[a-zA-Z]+[0-9]+$";
+
+        /// <summary>
         /// 
         /// </summary>
         /// <param name="v"></param>
         /// <returns></returns>
         public delegate int Lookup(String v);
+
         /// <summary>
         /// 
         /// </summary>
@@ -26,6 +33,16 @@ namespace FormulaEvaluator
         public static int Evaluate(String exp, Lookup variableEvaluator)
         {
             return 0;
+        }
+
+        /// <summary>
+        /// Checks if the passed parameter is a valid variable name.
+        /// </summary>
+        /// <param name="varName">Variable name to check</param>
+        /// <returns>True if valid else False</returns>
+        public static bool IsValidVar(String varName)
+        {
+            return Regex.IsMatch(varName, VALID_VAR_PATTERN);
         }
     }
 }
