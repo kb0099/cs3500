5464d2457038a06a08ab9897917884024d786c20
Kedar B <pra_ked@live.com>
2015-09-07 20:49:11 -0600

    Edit XML Doc comment for Evaluator class

--- PS1/FormulaEvaluator/Evaluator.cs
+++ PS1/FormulaEvaluator/Evaluator.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace FormulaEvaluator
 {
     /// <summary>
-    /// 
+    /// Represents a static class that acts as a container for static members.
     /// </summary>
     public static class Evaluator
     {
