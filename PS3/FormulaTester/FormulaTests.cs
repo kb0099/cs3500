bb8fc2846f57832de0645755235e93437df13dbd
Kedar B <pra_ked@live.com>
2015-09-24 18:03:48 -0600

    PS3: Adds FormulaTester Project

---
+++ PS3/FormulaTester/FormulaTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpreadsheetUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpreadsheetUtilities
+{
+    [TestClass()]
+    public class FormulaTests
+    {
+        /// <summary>
+        /// Verify that the only tokens are (, ), +, -, *, /, 
+        /// variables, and floating-point numbers.
+        /// </summary>
+        [TestMethod()]
+        public void PublicFormulaTestParsing()
+        {
+            Assert.Fail();
+        }
+
+        /// <summary>
+        /// Verifies One Token Rule
+        /// There must be at least one token.
+        /// </summary> 
+        [TestMethod()]
+        public void PublicFormulaTestAtLeastOneToken()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PublicEvaluateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PublicGetVariablesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PublicToStringTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PublicEqualsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PublicGetHashCodeTest()
+        {
+            Assert.Fail();
+        }
+        
+    }
+}
