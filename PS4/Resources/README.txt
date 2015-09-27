dceb98a41890ef7d529fd5c26a8280076593ecaa
RockerMAT <rocker.mat@live.com>
2015-09-27 15:49:02 -0600

    PS4: Creation of the projects used in the assignment

---
+++ PS4/Resources/README.txt
@@ -0,0 +1,3 @@
+﻿Mitchell Terry 9/27/2015
+Using the final implementations of PS2 and PS3 from 9/10/2015 and 9/17/2015 respectively
+Plan of implementing PS4 by using the DependencyGraph of PS2 and Formula of PS3
---
+++ PS4/Spreadsheet/Spreadsheet.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpreadsheetUtilities;
+
+namespace SS
+{
+    public class Spreadsheet : AbstractSpreadsheet
+    {
+        /// <summary>
+        /// Create an empty Spreadsheet
+        /// </summary>
+        public Spreadsheet()
+        {
+
+        }
+
+        public override IEnumerable<string> GetNamesOfAllNonemptyCells()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object GetCellContents(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ISet<string> SetCellContents(string name, double number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ISet<string> SetCellContents(string name, string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ISet<string> SetCellContents(string name, Formula formula)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IEnumerable<string> GetDirectDependents(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
