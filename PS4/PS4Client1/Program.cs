4b06628cf171e68c77a7436e9ef65ed43f14a7c6
Kedar B <pra_ked@live.com>
2015-10-27 19:44:10 -0600

    Adds auxiliary tests and clients previously ignored

---
+++ PS4/PS4Client1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PS4Client1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XElement spreadsheet = new XElement("spreadsheet", new XAttribute("version", "test"),
+                new XElement("cell",  new XElement("name", "a1"), new XElement("contents", "9")),
+                new XElement("cell", new XElement("name", "b1"), new XElement("contents", "10")),
+                new XElement("cell", new XElement("name", "c1"), new XElement("contents", "=a1+b1"))
+                );
+            spreadsheet.Save("kb01.xml");
+            Console.WriteLine("done!");
+        }
+    }
+}
