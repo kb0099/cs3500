933a5c28c08c941cb41843d8fc05131f04487fae
RockerMAT <rocker.mat@live.com>
2015-11-07 14:45:01 -0700

    PS7: added elements to GameForm, worked out way to have connection and game panels hide/show (not removing LoginForm for now)

--- PS7/View/GameForm.Designer.cs
+++ PS7/View/GameForm.Designer.cs
@@ -28,20 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ConnectionPanel = new System.Windows.Forms.Panel();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.ServerTextBox = new System.Windows.Forms.TextBox();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.ServerLabel = new System.Windows.Forms.Label();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.GamePanel = new System.Windows.Forms.Panel();
+            this.ConnectionPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // ConnectionPanel
+            // 
+            this.ConnectionPanel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ConnectionPanel.Controls.Add(this.ConnectButton);
+            this.ConnectionPanel.Controls.Add(this.ServerLabel);
+            this.ConnectionPanel.Controls.Add(this.NameLabel);
+            this.ConnectionPanel.Controls.Add(this.ServerTextBox);
+            this.ConnectionPanel.Controls.Add(this.NameTextBox);
+            this.ConnectionPanel.Location = new System.Drawing.Point(442, 260);
+            this.ConnectionPanel.Name = "ConnectionPanel";
+            this.ConnectionPanel.Size = new System.Drawing.Size(380, 160);
+            this.ConnectionPanel.TabIndex = 0;
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameTextBox.Location = new System.Drawing.Point(118, 4);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(259, 44);
+            this.NameTextBox.TabIndex = 0;
+            // 
+            // ServerTextBox
+            // 
+            this.ServerTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ServerTextBox.Location = new System.Drawing.Point(118, 54);
+            this.ServerTextBox.Name = "ServerTextBox";
+            this.ServerTextBox.Size = new System.Drawing.Size(259, 44);
+            this.ServerTextBox.TabIndex = 1;
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameLabel.Location = new System.Drawing.Point(3, 7);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(103, 37);
+            this.NameLabel.TabIndex = 2;
+            this.NameLabel.Text = "Name";
+            // 
+            // ServerLabel
+            // 
+            this.ServerLabel.AutoSize = true;
+            this.ServerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ServerLabel.Location = new System.Drawing.Point(3, 57);
+            this.ServerLabel.Name = "ServerLabel";
+            this.ServerLabel.Size = new System.Drawing.Size(109, 37);
+            this.ServerLabel.TabIndex = 3;
+            this.ServerLabel.Text = "Server";
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConnectButton.Location = new System.Drawing.Point(115, 104);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(150, 50);
+            this.ConnectButton.TabIndex = 4;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // GamePanel
+            // 
+            this.GamePanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.GamePanel.Location = new System.Drawing.Point(12, 12);
+            this.GamePanel.Name = "GamePanel";
+            this.GamePanel.Size = new System.Drawing.Size(1240, 657);
+            this.GamePanel.TabIndex = 1;
+            this.GamePanel.Visible = false;
+            this.GamePanel.Paint += new System.Windows.Forms.PaintEventHandler(this.GamePanel_Paint);
+            // 
+            // GameForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(783, 451);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.ConnectionPanel);
+            this.Controls.Add(this.GamePanel);
+            this.Name = "GameForm";
+            this.Text = "AgCubio";
+            this.ConnectionPanel.ResumeLayout(false);
+            this.ConnectionPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel ConnectionPanel;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.Label ServerLabel;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.TextBox ServerTextBox;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Panel GamePanel;
     }
 }
 
--- PS7/View/GameForm.cs
+++ PS7/View/GameForm.cs
@@ -12,9 +12,36 @@ namespace View
 {
     public partial class GameForm : Form
     {
+        private System.Drawing.SolidBrush brush;
+
         public GameForm()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// The method run when the "Connect" button is clicked.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            // TODO a test on being able to change panels displayed; the connect panel can be visible first, then when the connection works it is hidden and the game panel is shown (which will start painting)
+            this.ConnectionPanel.Hide();
+            this.GamePanel.Show();
+        }
+
+        /// <summary>
+        /// The paint method run to paint the game content.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GamePanel_Paint(object sender, PaintEventArgs e)
+        {
+            // TODO a test on painting in panel
+            Color color = Color.FromArgb(255, 0, 255);
+            brush = new System.Drawing.SolidBrush(color);
+            e.Graphics.FillRectangle(brush, new Rectangle(20, 50, 500, 480));
         }
     }
 }
