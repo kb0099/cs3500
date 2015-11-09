362c0f195c7af84835f4363198f8e57dc45927bb
Kedar B <pra_ked@live.com>
2015-11-08 23:14:15 -0700

    PS7: Updates the Network class functionality

--- PS7/Network_Controller/Network.cs
+++ PS7/Network_Controller/Network.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Sockets;
+using System.Net;
 
 namespace AgCubio
 {
     public static class Network
     {
-        static PreservedState preservedStateObject = new PreservedState();
-        
+        // Represents a default port (const are static by default!)
+        public const int DEFAULT_PORT = 11000;
+
         /// <summary>
         /// This function should attempt to connect to the server via a provided hostname. 
         /// It should save the callback function (in a state object) for use when data arrives.
@@ -23,45 +25,46 @@ namespace AgCubio
         /// connection is made</param>
         /// <param name="hostname">the name of the server to connect to</param>
         /// <returns></returns>
-        Socket ConnectToServer(Action callback, string hostname)
+        static Socket ConnectToServer(Action<PreservedState> callback, string hostname, int port = DEFAULT_PORT)
         {
             try
             {
-                // Establish the remote endpoint for the socket.
-                // The name of the 
-                // remote device is "host.contoso.com".
-                IPHostEntry ipHostInfo = Dns.Resolve("host.contoso.com");
+                // Instantiate the remote endpoint for the socket.
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(hostname);
                 IPAddress ipAddress = ipHostInfo.AddressList[0];
                 IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
 
                 // Create a TCP/IP socket.
-                Socket client = new Socket(AddressFamily.InterNetwork,
-                    SocketType.Stream, ProtocolType.Tcp);
+                Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
                 // Connect to the remote endpoint.
-                client.BeginConnect(remoteEP,
-                    new AsyncCallback(ConnectCallback), client);
-                connectDone.WaitOne();
+                clientSocket.BeginConnect(remoteEP, new AsyncCallback(ConnectedToServer), new PreservedState() { clientSocket = clientSocket, callback = callback });
 
-                // Send test data to the remote device.
-                Send(client, "This is a test<EOF>");
-                sendDone.WaitOne();
+                return clientSocket;
 
-                // Receive the response from the remote device.
-                Receive(client);
-                receiveDone.WaitOne();
+                //connectDone.WaitOne();
 
-                // Write the response to the console.
-                Console.WriteLine("Response received : {0}", response);
+                //// Send test data to the remote device.
+                //Send(client, "This is a test<EOF>");
+                //sendDone.WaitOne();
 
-                // Release the socket.
-                client.Shutdown(SocketShutdown.Both);
-                client.Close();
+                //// Receive the response from the remote device.
+                //Receive(client);
+                //receiveDone.WaitOne();
+
+                //// Write the response to the console.
+                //Console.WriteLine("Response received : {0}", response);
+
+                //// Release the socket.
+                //client.Shutdown(SocketShutdown.Both);
+                //client.Close();
 
             }
             catch (Exception e)
             {
+                callback(new PreservedState() { errorMsg = e.Message });
                 Console.WriteLine(e.ToString());
+                return null;
             }
         }
 
@@ -75,8 +78,23 @@ namespace AgCubio
         /// arrive (and provide the ReceiveCallback function for this purpose)
         /// </summary>
         /// <param name="ar"></param>
-        void ConnectedToServer(IAsyncResult ar)
+        static void ConnectedToServer(IAsyncResult ar)
         {
+            // Retrieve the PreservedState object
+            PreservedState stateObj = (PreservedState)ar.AsyncState;
+            try
+            {
+                // Complete the connection.
+                stateObj.clientSocket.EndConnect(ar);
+                stateObj.callback(stateObj);
+                WantMoreData(stateObj);
+            }
+            catch (Exception ex)
+            {
+                stateObj.errorMsg = ex.Message;
+                stateObj.callback(stateObj);
+                Console.WriteLine(ex.ToString());
+            }
         }
 
         /// <summary>
@@ -87,10 +105,22 @@ namespace AgCubio
         /// For our purposes, this function should not request more data. 
         /// It is up to the code in the callback function above to request more data.
         /// </summary>
-        /// <param name="state_in_an_ar_object"></param>
-        void ReceiveCallback(IAsyncResult ar)
+        /// <param name="ar"></param>
+        static void ReceiveCallback(IAsyncResult ar)
         {
-
+            try
+            {
+                PreservedState state = (PreservedState)ar.AsyncState;
+                int count = state.clientSocket.EndReceive(ar);
+                if (count <= 0)
+                    return;
+                state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, count));
+                state.callback(state);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
 
         /// <summary>
@@ -98,22 +128,34 @@ namespace AgCubio
         /// whenever it wants more data. 
         /// Note: the client will probably want more data every time it gets data.
         /// </summary>
-        /// <param name="psObject"></param>
-        void WantMoreData(PreservedState psObject)
+        /// <param name="state"></param>
+        static void WantMoreData(PreservedState state)
         {
-
+            state.clientSocket.BeginReceive(state.buffer, 0, 1024, SocketFlags.None, new AsyncCallback(ReceiveCallback), (object)state);
         }
 
-       
+
         /// <summary>
         /// This function (along with it's helper 'SendCallback') will allow a 
         /// program to send data over a socket. This function needs to convert 
-        /// the data into bytes and then send them using socket.BeginSend.
+        /// the data into bytes and then send them using socket.
         /// </summary>
         /// <param name="socket"></param>
         /// <param name="data"></param>
-        void Send(Socket socket, String data)
+        static void Send(Socket socket, String data)
         {
+            // Convert the string data to byte data using ASCII encoding.
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
+            try
+            {
+                // Begin sending the data to the remote device.
+                socket.BeginSend(byteData, 0, byteData.Length, SocketFlags.None, new AsyncCallback(SendCallBack), socket);
+            }
+            catch
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
         }
 
 
@@ -122,9 +164,16 @@ namespace AgCubio
         /// then life is good and nothing needs to be done If there is more data to send, 
         /// the SendCallBack needs to arrange to send this data 
         /// </summary>
-        void SendCallBack()
+        static void SendCallBack(IAsyncResult ar)
         {
-
+            try
+            {
+                ((Socket)ar.AsyncState).EndSend(ar);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
