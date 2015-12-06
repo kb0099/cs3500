00123848a81fff7f8b6e29213d3e098e095004ca
kb0099 <u0574564@utah.edu>
2015-12-06 16:34:58 -0700

    PS9: Adds ExitHelper to help run code as console exits

---
+++ PS7/Server/ExitHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class ExitHelper
+    {
+        [System.Runtime.InteropServices.DllImport("Kernel32")]
+        private static extern bool SetConsoleCtrlHandler(EventHandler handler, bool add);
+        private delegate bool EventHandler(byte b);
+
+        private Action OnExit;
+        /// <summary>
+        /// Helper to execute code after user clicks the close button.
+        /// </summary>
+        /// <param name="onExit">Action to execute at the program termination.</param>
+        public ExitHelper(Action onExit)
+        {
+            OnExit += onExit;
+            SetConsoleCtrlHandler((ctrl) =>
+            {
+                OnExit(); return true;
+            }, true);
+        }
+    }
+}
